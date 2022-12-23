6ffab405b2daec55df127bb5a8f94a5d27c0222b
classall <corentin.lssll@gmail.com>
2022-12-23 19:05:01 +0100

    Compta base features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'inde-compta';
+  displayedColumns: string[] = ['date', 'label', 'amount', 'TVA'];
+  dataSource: { date: string, label: string, amount: number }[] = [];
+  CA_HT = 0;
+  CA_TTC = 0;
+  TVA = 0;
+  benef = 0;
+  URSSAF = 0;
+
+  onFileUpload(event: any) {
+    const reader = new FileReader();
+    reader.readAsText(event.target.files[0]);
+    reader.onload = () => {
+      const text = reader.result as string;
+      this.csvToJSON(text);
+    };
+  }
+
+  csvToJSON(csvText: string) {
+    const lines = csvText.replace(/\r/gm, '').split("\n");
+    const result = [];
+    const headers = lines[0].split(";");
+
+    for (let i = 1; i < lines.length - 1; i++) {
+      const obj: any = {};
+      const currentline = lines[i].split(";");
+
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      const tr: any = {
+        date: obj['Date'],
+        label: obj['Libellé'],
+        amount: Number(obj['Solde mouvement'].replace(',', '.')),
+        credit: Number(obj['Crédit'].replace(',', '.')),
+        debit: Number(obj['Débit'].replace(',', '.')),
+        TVA: Number(obj['Montant de TVA total'].replace(',', '.'))
+      };
+
+      if (!isNaN(tr.credit)) {
+        tr.credit_ht = tr.credit / 1.2;
+      }
+
+      result.push(tr);
+      this.CA_HT += tr.credit_ht;
+      this.CA_TTC += tr.credit;
+      this.TVA += tr.TVA;
+      this.benef += tr.amount;
+    }
+    this.URSSAF = Math.round(this.CA_HT * 0.222);
+    this.dataSource = result;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import localeFr from '@angular/common/locales/fr';
+import {BrowserModule} from '@angular/platform-browser';
+import {registerLocaleData} from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -10,9 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'},
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
